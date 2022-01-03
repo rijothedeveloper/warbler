999317e113119dd2c149d38d14694a85c3ba37c6
Rijo George <rijogeorge@rijos-mbp.lan>
2022-01-02 17:59:23 -0800

    Step Two: Fix Logout

--- app.py
+++ app.py
@@ -112,8 +112,9 @@ def login():
 @app.route('/logout')
 def logout():
     """Handle logout of user."""
-
-    # IMPLEMENT THIS
+    do_logout()
+    flash("logged out")
+    return redirect("/login")
 
 
 ##############################################################################
